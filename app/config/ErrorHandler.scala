436f3224666f12ee0e9015846b024bbe367f7192
tomvdv59 <t.vandevelde@yahoo.fr>
2017-10-02 21:39:56 +0100

    error handler

---
+++ app/config/ErrorHandler.scala
@@ -0,0 +1,24 @@
+package config
+
+import javax.inject.Singleton
+
+import models.{ErrorInternalServerError, ErrorInvalidRequest}
+import play.api.Logger
+import play.api.http.DefaultHttpErrorHandler
+import play.api.libs.json.Json
+import play.api.mvc.{RequestHeader, Result, Results}
+
+import scala.concurrent.Future
+
+@Singleton
+class ErrorHandler extends DefaultHttpErrorHandler {
+  override def onBadRequest(request: RequestHeader, message: String): Future[Result] = {
+    Future.successful(Results.BadRequest(Json.toJson(ErrorInvalidRequest(message)).toString()))
+  }
+
+  override def onServerError(request: RequestHeader, exception: Throwable) = {
+    Logger.error("An unexpected error occurred", exception)
+    Future.successful(ErrorInternalServerError(exception.getMessage).toHttpResponse)
+  }
+
+}
--- app/models/Errors.scala
+++ app/models/Errors.scala
@@ -1,6 +1,6 @@
 package models
 
-import play.api.http.Status.{BAD_REQUEST, NOT_FOUND}
+import play.api.http.Status.{BAD_REQUEST, INTERNAL_SERVER_ERROR, NOT_FOUND}
 import play.api.libs.json.Json
 import play.api.mvc.{Result, Results}
 import models.JsonFormatters._
@@ -13,6 +13,7 @@ sealed abstract class ErrorResponse(
   def toHttpResponse: Result = Results.Status(httpStatusCode)(Json.toJson(this))
 }
 
+case class ErrorInternalServerError(errorMessage: String) extends ErrorResponse(INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", errorMessage)
 case class ErrorInvalidRequest(errorMessage: String) extends ErrorResponse(BAD_REQUEST, "INVALID_REQUEST", errorMessage)
 case object RequestedAuthorityNotFound extends ErrorResponse(NOT_FOUND, "NOT_FOUND", s"requested authority not found")
 
