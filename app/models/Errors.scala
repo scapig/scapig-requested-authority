da93af665b27d48a2247a817ba20fb33c4ef88db
tomvdv59 <t.vandevelde@yahoo.fr>
2017-10-04 23:39:17 +0100

    fix error handler

--- app/models/Errors.scala
+++ app/models/Errors.scala
@@ -16,6 +16,7 @@ sealed abstract class ErrorResponse(
 case class ErrorInternalServerError(errorMessage: String) extends ErrorResponse(INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", errorMessage)
 case class ErrorInvalidRequest(errorMessage: String) extends ErrorResponse(BAD_REQUEST, "INVALID_REQUEST", errorMessage)
 case object RequestedAuthorityNotFound extends ErrorResponse(NOT_FOUND, "NOT_FOUND", s"requested authority not found")
+case class ErrorNotFound() extends ErrorResponse(NOT_FOUND, "NOT_FOUND", "The resource could not be found.")
 
 class RequestedAuthorityNotFoundException(message: String) extends RuntimeException(message)
 class ValidationException(message: String) extends RuntimeException(message)
