01aade62632bd8fd37c56eec69ed34e5054897db
Thomas VANDEVELDE <thomas.vandevelde@digital.hmrc.gov.uk>
2018-01-17 23:00:59 +0000

    update docker file

--- Dockerfile
+++ Dockerfile
@@ -6,3 +6,5 @@ RUN chmod +x start-docker.sh
 RUN tar xvf scapig-requested-authority-*.tgz
 
 EXPOSE 7060
+
+CMD ["sh", "start-docker.sh"]
--- start-docker.sh
+++ start-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 SCRIPT=$(find . -type f -name scapig-requested-authority)
 rm -f scapig-requested-authority*/RUNNING_PID
-exec $SCRIPT -Dhttp.port=7060
+exec $SCRIPT -Dhttp.port=7060 -J-Xms128M -J-Xmx512m
